79f50a3f40ed8977027368027562ef28a9f7ec45
Jairo Alberto Pérez Ovares <jairo.perezovares31@gmail.com>
2026-01-15 18:15:54 -0600

    Replace test_logc.py with v0.2.0

--- tests/test_logc.py
+++ tests/test_logc.py
@@ -1,10 +1,12 @@
-from logc_tool.logc import parse_arguments, curate_and_standardize_cli_input
+from logc_tool import logc
 import pytest
+import re
 
-# test the basic parameters for the parser work just fine
+# Test proper functionality and argument assignment
 def test_basic_parsing():
     test_args = ["-s", "test-logs.tgz", "-d", "output.txt", "-f", "chassisd", "-t", "Oct  6 to Oct  8"]
-    parsed = parse_arguments(test_args)
+
+    parsed = logc.parse_args(test_args)
 
     assert parsed.source == "test-logs.tgz"
     assert parsed.destination == "output.txt"
@@ -12,19 +14,37 @@ def test_basic_parsing():
     assert parsed.time == "Oct  6 to Oct  8"
     assert parsed.ignore_case == False
 
-# test the tool needs at least -s, -d, -f, and either -t or any key flag
+# Test when the user hasn't specified all required flags
 def test_missing_flag():
     test_args = ["-s", "test-logs.tgz", "-d", "output.txt", "-f", "chassisd"]
 
-    # it exits with the "Please, specify at least -t or -k, or both." message
-    with pytest.raises(SystemExit):
-        parsed = parse_arguments(test_args)
-        curate_and_standardize_cli_input(parsed)
+    with pytest.raises(TypeError):
+        logc.parse_args(test_args)
 
-# test the parser rejects mutually exclusive keys
+# Test when using both AND and OR operators at the same time
 def test_mutually_exclusive_keys():
-    test_args = ["-s", "test-logs.tgz", "-d", "output.txt", "-f", "chassisd", "-k", "banana", "-ka", "banana", "apple"]
+    test_args = ["-s", "test-logs.tgz", "-d", "output.txt", "-f", "chassisd", "-k", "fpc", "and", "pic", "or", "port"]
 
-    with pytest.raises(SystemExit):
-        parsed = parse_arguments(test_args)
+    parsed = logc.parse_args(test_args)
+
+    with pytest.raises(logc.BothOperatorsError):
+        logc.parse_by_key([], parsed.key)
+
+# Test when the wildcard is malformed or invalid
+def test_invalid_wildcard():
+    test_args = ["-s", "test-logs.tgz", "-d", "output.txt", "-w", "*", "-k", "fpc"]
+
+    parsed = logc.parse_args(test_args)
+
+    with pytest.raises(re.PatternError):
+        logc.get_wildcard_files(parsed.source, parsed.wildcard)
+
+# Test for no operators within args.key
+def test_no_operator_for_key():
+    test_args = ["-s", "test-logs.tgz", "-d", "output.txt", "-w", "messages", "-k", "fpc", "pic", "port"]
+
+    parsed = logc.parse_args(test_args)
+
+    with pytest.raises(logc.NoOperatorError):
+        logc.parse_by_key([], parsed.key)
 
